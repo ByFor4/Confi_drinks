8f7d441d66249a5eaa749b67f6c2c7973a1fe98f
ByFor4 <by.for4@gmail.com>
2024-05-24 16:48:12 +0300

    dell_reg_log

--- Confi_drinks1/main/urls.py
+++ Confi_drinks1/main/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.main, name = "home"),
-    path("reg",views.reg, name = "reg"),
     path("cm",views.cm, name = "cm"),
     path("search_of_ingrid",views.search_of_ingrid, name = "search_of_ingrid"),
     path("prof",views.prof, name = "prof"),
-    path("log",views.log, name = "log")
+    path("coctail_info",views.coctail_info, name = "coctail_info"),
 ]
--- Confi_drinks1/main/views.py
+++ Confi_drinks1/main/views.py
@@ -6,22 +6,6 @@ from .forms import userForms
 def main(request):
     return render(request,'main/main2.html')
 
-def reg(request):
-    error = ""
-    if request.method == "POST":
-        form = userForms(request.POST)
-        if form.is_valid():
-            form.save()
-        else:
-            error = "некоректный имя пользователя или пороль"
-
-    form = userForms()
-    data = {
-        "form": form,
-        "error": error
-    }
-    return render(request,'main/registration.html', data)
-
 def cm(request):
     return render(request,'main/coctails_make.html')
 
@@ -31,5 +15,5 @@ def search_of_ingrid(request):
 def prof(request):
     return render(request, "main/my_profile.html")
 
-def log(request):
-    return render(request, "main/login.html")
+def coctail_info(request):
+    return render(request, "main/coctail_info.html")
